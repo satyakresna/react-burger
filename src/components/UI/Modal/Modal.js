cc1fc95ae76b1115ca37df9c0bcf48a4b940c40f
Kresna <satyakresna6295@gmail.com>
2020-12-15 19:08:03 +0800

    use React.memo in Modal components

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import styles from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-                <div
-                    className={styles.Modal}
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
-                    {this.props.children}
-                </div>
-            </React.Fragment>
-        )
-    }
+const Modal = (props) => {
+    return (
+        <React.Fragment>
+            <Backdrop show={props.show} clicked={props.modalClosed} />
+            <div
+                className={styles.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                {props.children}
+            </div>
+        </React.Fragment>
+    )
 }
 
-export default Modal;
+const isEqual = (prevProps, nextProps) => {
+    return nextProps.show !== prevProps.show || nextProps.children !== prevProps.children;
+}
+
+export default React.memo(Modal, isEqual)
